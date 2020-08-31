44e657dc29961fd74ec9d6f9844062c91bcef20b
miZye <mi.zyeman@gmail.com>
2020-08-31 10:36:23 +0200

    Initial Commit

---
+++ Import-Pictures/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Import-Pictures
+//
+//  Created by Macbook Mike Zye on 31.08.20.
+//  Copyright © 2020 MikeZyeman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Import-Pictures/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Import-Pictures
+//
+//  Created by Macbook Mike Zye on 31.08.20.
+//  Copyright © 2020 MikeZyeman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
